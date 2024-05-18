0b0a4eab08bec2b3bf4d752c6e8b1a9337ae0b61
anapereirar <64859363+anapereirar@users.noreply.github.com>
2024-05-17 20:33:32 -0500

    Primer tema

--- app/components/sidebar/page.tsx
+++ app/components/sidebar/page.tsx
@@ -14,13 +14,16 @@ export default function Sidebar(){
         <nav className="text-base lg:text-sm">
               <ul role="list" className="space-y-9">
                 <li>
-                  <h2 className="font-display font-medium text-slate-900 dark:text-white">Introducció a la POO<nav></nav></h2>
+                  <h2 className="font-display font-medium text-slate-900 dark:text-white">Introducción a la POO<nav></nav></h2>
                   <ul role="list" className="mt-2 space-y-2 border-l-2 border-slate-100 lg:mt-4 lg:space-y-4 lg:border-slate-200 dark:border-slate-800">
                     <li className="relative">
                       <a className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full font-semibold text-sky-500 before:bg-sky-500" href="/">Clases y objetos</a>
                     </li>
                     <li className="relative">
-                      <a className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300" href="/docs/installation">Installation</a>
+                      <a className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300" href="/docs/installation">Atributos y métodos</a>
+                    </li>
+                    <li className="relative">
+                      <a className="block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300" href="/docs/installation">Constructores</a>
                     </li>
                   </ul>
                 </li>
